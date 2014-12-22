4ce3ffa5d101f9aaa241b5bac7d31dc2facfbc9f
Jonathan Zong <jonathan@jonathanzong.com>
2014-12-21 21:12:08 -0600

    packery layouts

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Blackbird
 
 Thirteen Ways of Looking at a Blackbird -- Wallace Stevens
 
+Digital art/experimentation with SVG + Javascript
+
 http://24.media.tumblr.com/0751b1f672c196709723cecbe2b753e6/tumblr_mtsyxjt4651qg2nqto1_250.gif
--- js/main.js
+++ js/main.js
@@ -1,29 +1,70 @@
-$(document).ready(function() {
-	var tweenCache = {};
-	$(".panel").click(function(e) {
-			var el = $(this).parent(".columns");
-			if (!el.hasClass("expanded")) {
-				$(".expanded").find(".panel").click();
-				el.addClass("expanded");
-				var siblings = el.parent().find(".columns").not(el);
-				siblings.hide();
-				var id = el.find(".panel").attr("id");
-				var w = el.parent(".row").width();
-				tweenCache[id] = TweenMax.to(el, 0.5, {
-					width: w,
-					height: w * 0.75,
-					onReverseComplete: function() {
-						siblings.show();
-					}
-				});
-			}
-			else {
-				var id = el.find(".panel").attr("id");
-				if (tweenCache[id]) {
-					tweenCache[id].reverse();
-					tweenCache[id] = null;
-				}
-				el.removeClass("expanded");
-			}
-		});
+//http://codepen.io/desandro/pen/paFEH
+
+// get vendor transition property, i.e. WebkitTransition
+var transitionProp = getStyleProperty('transition');
+// get transition end event name
+var transitionEndEvent = {
+  WebkitTransition: 'webkitTransitionEnd',
+  MozTransition: 'transitionend',
+  OTransition: 'otransitionend',
+  transition: 'transitionend'
+}[ transitionProp ];
+
+$( function() {
+  var $container = $('#container').packery({
+  	isHorizontal: false,
+    gutter: ".gutter-sizer",
+  	itemSelector: ".item",
+  	columnWidth: ".grid-sizer"
+  });
+  var pckry = $container.data('packery');
+  var inOrder = $('.item');
+  var len = inOrder.length;
+  
+  $container.on( 'click', '.item-content', function( event ) {
+    var target = this;
+    var $target = $( this );
+
+    // disable transition
+    $target.css( transitionProp, 'none' );
+    // set current size
+    $target.css({
+      width: $target.width(),
+      height: $target.height()
+    });
+
+    var $itemElem = $target.parent();
+    var isExpanded = $itemElem.hasClass('is-expanded');
+    $itemElem.toggleClass('is-expanded');
+    // force redraw
+    var redraw = target.offsetWidth;
+    // renable default transition
+    target.style[ transitionProp ] = '';
+
+    // reset 100%/100% sizing after transition end
+    if ( transitionProp ) {
+      $target.one( transitionEndEvent, function() {
+        target.style.width = '';
+        target.style.height = '';
+      });
+    }
+
+    // set new size
+    $target.css({
+      width: $itemElem.width(),
+      height: $itemElem.height()
+    });
+
+    if ( isExpanded ) {
+      // if shrinking, just layout
+      for (var i = 0; i < len; i++) {
+        pckry.items[i].element = inOrder[ i ];
+      }
+      $container.packery();
+    } else {
+      // if expanding, fit it
+      $container.packery( 'fit', $itemElem[0] );
+    }
+
+  });
 });
